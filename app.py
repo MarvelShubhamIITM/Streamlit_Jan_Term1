414aa1ee8d07960b5a75e8d8591c5b42a35a7132
MarvelShubhamIITM <165567256+MarvelShubhamIITM@users.noreply.github.com>
2024-03-31 19:31:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 def addition(a,b):
   return(a+b)
 st.title("Little Calculator")
-a = st.number_input()
-b = st.number_input()
+a = st.number_input("Input your first number : ")
+b = st.number_input("Input your second number : ")
 
 st.write(addition(a,b))
+st.write(answer)
